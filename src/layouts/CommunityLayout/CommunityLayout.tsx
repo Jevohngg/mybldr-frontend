170af735f5c54d2ed96eee24acbb9cc03c7b5211
Jevohngg <jevohngentry@gmail.com>
2025-12-17 09:24:05 -0700

    Updated CommunityLayout.tsx

--- src/layouts/CommunityLayout/CommunityLayout.tsx
+++ src/layouts/CommunityLayout/CommunityLayout.tsx
@@ -1,15 +1,5 @@
-import { Outlet, useParams } from 'react-router-dom'
-import CommunitySideNav from '../../navigation/CommunitySideNav/CommunitySideNav'
-import styles from './CommunityLayout.module.css'
+import { Outlet } from 'react-router-dom'
 
 export default function CommunityLayout() {
-  const { communityId } = useParams()
-  return (
-    <div className={styles.wrap}>
-      <CommunitySideNav communityId={communityId || ''} />
-      <div className={styles.content}>
-        <Outlet />
-      </div>
-    </div>
-  )
+  return <Outlet />
 }
